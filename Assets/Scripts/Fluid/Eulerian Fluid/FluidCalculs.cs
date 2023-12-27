af993e5e783c50221124678bbf708c8316338267
ALEX <lescot.alexg@gmail.com>
2023-12-27 15:11:50 +0100

    mousepos ... pas resolut

--- Assets/Scripts/Fluid/Eulerian Fluid/FluidCalculs.cs
+++ Assets/Scripts/Fluid/Eulerian Fluid/FluidCalculs.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class FluidCalculs : MonoBehaviour
 {
-    public int n = 32;
-
-    private Texture2D texture;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        texture = new Texture2D(n+2, n+2, TextureFormat.RGBAHalf, false);
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Fluid/Eulerian Fluid/FluidRender.cs
+++ Assets/Scripts/Fluid/Eulerian Fluid/FluidRender.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FluidRender : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material material;
+    public int n = 32;
+    public Camera mainCam;
+    
+    private Texture2D texture;
+    private int _size;
+    
+    /* TESTS */
+    public Vector3 _mousepositon;
+
     void Start()
     {
+        mainCam = Camera.main;
+        texture = new Texture2D(n+2, n+2, TextureFormat.RGBAHalf, false);
+        material.SetTexture("_MainTex", texture);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 camPos = Mouse.current.position.ReadValue();
+        _mousepositon = mainCam.ScreenToWorldPoint(camPos);
     }
+    Vector3 GetMousePos()
+    {
+        Vector3 camPos = Mouse.current.position.ReadValue();
+        Vector3 p = mainCam.ScreenToWorldPoint(camPos);
+        return p;
+        /* return new Vector3(p.x, 0f, p.z); */
+    }
+    
 }
